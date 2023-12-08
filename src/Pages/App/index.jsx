c21a241a2bf6de741c2cd85f0813eff0e134b7bd
adan_due <ing.adanduenas@gmail.com>
2023-12-08 17:06:56 -0600

    Creating the project

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,26 @@
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SingIn from '../SingIn'
+import Home from '../home'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-100'>
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SingIn />
+      </div>
+    </>
+  )
+}
+
+export default App
