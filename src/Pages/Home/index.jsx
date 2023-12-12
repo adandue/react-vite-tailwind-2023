a2846991c018add1d9ea3773ae35eca0cfb92921
adan_due <ing.adanduenas@gmail.com>
2023-12-12 13:50:41 -0600

    Consumiendo la FakeStore API para pintar cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
+import { apiUrl } from "../../api"
 
 const Home = () => {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${apiUrl}/products`)
+                const data = await response.json()
+                setItems(data)
+            } catch (error) {
+                console.error(`Ocurrió un error: ${error}`);
+            }
+        }
+        fetchData();
+    }, []);
 return (
     <>
         <Layout>
         Home
-        <Card />
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+            items?.map((item) => (
+            <Card 
+            key={item.id}
+            data={item}
+            />
+            )
+            )
+        }
+        </div>
         </Layout>
         
     </>
