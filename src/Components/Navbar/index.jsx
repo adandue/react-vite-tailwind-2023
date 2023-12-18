30c3093485d4dc575ccfd8e9c66933e885e36e12
adan_due <ing.adanduenas@gmail.com>
2023-12-18 14:57:18 -0600

    Contador de productos en el carrito

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from 'react-router-dom'
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
 
 const Navbar = () => {
+    const context = useContext(ShoppingCartContext)
     const activeStyle = 'underline underline-offset-4'
     return (
         <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white'>
@@ -108,7 +111,7 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li>
-                    C 0
+                    C {context.count}
                 </li>
             </ul>
         </nav>
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,5 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
-import { ShoppingCartProvider } from '../../context'
+import { ShoppingCartProvider } from '../../Context'
 import { MyAccount } from '../MyAccount'
 import { MyOrder } from '../MyOrder'
 import { MyOrders } from '../MyOrders'
