9111bb9d2bfd2b0bc2cc0de2eef12c89b1cffebb
adan_due <ing.adanduenas@gmail.com>
2023-12-21 13:14:37 -0600

    Evitando productis dupicados en el carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,39 +1,39 @@
-import { useContext } from 'react';
-import { XMarkIcon } from '@heroicons/react/24/solid';
-import { ShoppingCartContext } from '../../Context';
-import { OrderCard } from '../OrderCard';
-import './styles.css';
+import { useContext } from 'react'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context'
+import { OrderCard } from '../OrderCard'
+import './styles.css'
 
 const CheckoutSideMenu = () => {
-    const context = useContext(ShoppingCartContext);
-    console.log('Cart: ', context.cartProducts);
-    // console.log('PRODUCT TO SHOW: ', context.productToShow)
-    return(
-        <aside 
-        className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu scrollable-cards flex-col fixed right-0 border bg-white border-black rounded-lg`}>
-        <div className='flex justify-between items-center p-6'>
-            <h2 className='font-medium text-xl'>My Order</h2>
-            <div>
-                <XMarkIcon 
-                className='h-6 w-6 text-gray-900 cursor-pointer' 
-                onClick={() => context.closeCheckoutSideMenu()}
-                />
-            </div>
+const context = useContext(ShoppingCartContext)
+
+return (
+    <>
+    <aside
+    className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+    <div className='flex justify-between items-center p-6'>
+        <h2 className='font-medium text-xl'>My Order</h2>
+        <div>
+        <XMarkIcon
+            className='h-6 w-6 text-black cursor-pointer'
+            onClick={() => context.closeCheckoutSideMenu()} />
         </div>
-        <div className=' px-6'>
+    </div>
+    <div className='px-6 overflow-y-scroll'>
         {
-            context.cartProducts.map((product) => (
-                <OrderCard 
-                key={ product.id }
-                title={ product.title } 
-                imageUrl={ product.image } 
-                price={ product.price } 
-                />
-            ))
+        context.cartProducts.map(product => (
+            <OrderCard
+            key={product.id}
+            title={product.title}
+            imageUrl={product.image}
+            price={product.price}
+            />
+        ))
         }
-        </div>
-        </aside>
-    );
+    </div>
+    </aside>
+    </>
+)
 }
 
 export { CheckoutSideMenu }
