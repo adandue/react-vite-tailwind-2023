12db4a9f60a31859fe5cb099965c917f7f03db15
adan_due <ing.adanduenas@gmail.com>
2024-03-13 13:29:36 -0600

    Agregar estado local para mostrar la vista de crear cuenta o de hacer log in

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
 import { Layout } from "../../Components/Layout"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ShoppingCartContext } from "../../Context"
 
 const SignIn = () => {
     const context = useContext(ShoppingCartContext)
+    const [view, setView] = useState('user-info')
 
     //account
     const account = localStorage.getItem('account')
@@ -15,12 +16,10 @@ const SignIn = () => {
     const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
     const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
-return (
-    <>
-        <Layout>
-        <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
-        <div className="flex flex-col w-80">
-            <p>
+    const renderLogIn = () => {
+        return (
+            <div className="flex flex-col w-80">
+                <p>
                 <span className="font-light text-sm">Email: </span>
                 <span>{parsedAccount?.email}</span>
             </p>
@@ -42,12 +41,25 @@ return (
             </div>
             <button
             className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+            onClick={() => setView('create-user-info')}
             disabled={hasUserAnAccount}
             >
                 Sign Up
             </button>
+            </div>
+        )
+    }
 
-        </div>
+    const renderCreateUserInfo = () => {
+        //TODO: Create render view
+    }
+
+    const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn()
+return (
+    <>
+        <Layout>
+        <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+        {renderView()}
         </Layout>
         
     </>
