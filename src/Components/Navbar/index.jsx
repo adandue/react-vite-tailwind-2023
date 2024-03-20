6f7c92fe2a147045f2f17d9e6a8d21d4f3c19bb2
adan_due <ing.adanduenas@gmail.com>
2024-03-20 16:48:03 -0600

    Agregar página de MyAccount mostrando datos y editándolos

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,17 +7,17 @@ const Navbar = () => {
     const context = useContext(ShoppingCartContext)
     const activeStyle = 'underline underline-offset-4'
 
-    //Sign Out
+    // Sign Out
     const signOut = localStorage.getItem('sign-out')
     const parsedSignOut = JSON.parse(signOut)
     const isUserSignOut = context.signOut || parsedSignOut
-    //Account
+    // Account
     const account = localStorage.getItem('account')
     const parsedAccount = JSON.parse(account)
-    //Has an account
+    // Has an account
     const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
     const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
-    const hasUserAnAccount = !noAccountInLocalState || noAccountInLocalStorage
+    const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
     const handleSignOut = () => {
         const stringifiedSignOut = JSON.stringify(true)
@@ -29,51 +29,41 @@ const Navbar = () => {
         if (hasUserAnAccount && !isUserSignOut) {
             return (
                 <>
-                    <li className='text-black/60'>
-                    mail@example.com
-                </li>
-                <li>
-                    <NavLink
-                    to='/my-orders'
-                    className={( {isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }
-                    >
-                    My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                    to='/my-account'
-                    className={( {isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }
-                    >
-                    My Account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                    to='/sign-in'
-                    className={( {isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }
-                    onClick={() => handleSignOut()}
-                    >
-                    Sign out
-                    </NavLink>
-                </li>
+                    <li className='text-white'>
+                        {parsedAccount?.email}
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/my-orders'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}>
+                            My Orders
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/my-account'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}>
+                            My Account
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/sign-in'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}
+                            onClick={() => handleSignOut()}>
+                            Sign out
+                        </NavLink>
+                        </li>
                 </>
             )
         } else {
             return (
                 <li>
                     <NavLink
-                        to="sign-in"
+                        to="/sign-in"
                         className={({ isActive }) => isActive ? activeStyle : undefined }
-                        onClick={() => handleSignOut()}
-                    >
-                    Sign Out
+                        onClick={() => handleSignOut()}>
+                        Sign in
                     </NavLink>
                 </li>
             )
